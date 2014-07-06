ecb7f5bf08595f33889840eb65e77d66c78d4d8c
fbessho <iwabako@gmail.com>
2014-07-06 14:51:43 +0900

    Link subdirecdtory of .vim/

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/vim ~/.vim
+mkdir ~/.vim
+ln -sf ~/dotfiles/vim/autoload ~/.vim/autoload
+ln -sf ~/dotfiles/vim/colors ~/.vim/colors
+ln -sf ~/dotfiles/vim/doc ~/.vim/doc
+ln -sf ~/dotfiles/vim/plugin ~/.vim/plugin
+ln -sf ~/dotfiles/vim/syntax ~/.vim/syntax
+
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 
